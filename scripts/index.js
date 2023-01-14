4df0f6e74b0fbcaa9bf1fc148107502200232c57
raeannlytle <118645514+raeannlytle@users.noreply.github.com>
2023-01-14 16:22:11 +0000

    add javascript for edit button

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,7 @@ const initialCards = [
 		link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
 	},
 ];
+
+const profileEditButton = document.querySelector('#profile-edit-button');
+
+console.log(profileEditButton)
