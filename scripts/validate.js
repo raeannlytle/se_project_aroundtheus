6f8efe3e21e9264613cf5c083337652d749c50b5
Raeann Lytle <raelytle@gmail.com>
2023-02-06 20:50:23 -0500

    writing validation

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,1 +1,22 @@
-console.log("hello from validate.js");
+// enabling validation by calling enableValidation()
+// pass all the settings on call
+
+function enableValidation(options) {
+    const formElements = [...document.querySelectorAll(options.formSelector)];
+    formElements.forEach((formElements) => {
+        formElements.addEventListener("submit", (e) => {
+            e.preventDefault();
+        });
+    });
+}
+
+const config = {
+    formSelector: ".popup__form",
+    inputSelector: ".popup__input",
+    submitButtonSelector: ".popup__button",
+    inactiveButtonClass: "popup__button_disabled",
+    inputErrorClass: "popup__input_type_error",
+    errorClass: "popup__error_visible"
+  }
+
+enableValidation(config);
