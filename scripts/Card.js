e05c8ab230db492186a1651ade5d6402a1c42173
Raeann Lytle <raelytle@gmail.com>
2023-03-02 15:26:16 -0500

    remove validate from HTML

---
+++ scripts/Card.js
@@ -0,0 +1,5 @@
+export default class Card {
+  constructor({ name, link }) {
+   console.log({ name, link }); 
+  }
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+import Card from "./Card.js";
+
 const initialCards = [
 	{
 		name: "Yosemite Valley",
@@ -29,6 +31,13 @@ const initialCards = [
 		link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
 	}
 ];
+
+const cardData = {
+  name: "Lago di Braies",
+	link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+}
+
+const card = new Card(cardData);
 
 /* Elements */
 const profileEditButton = document.querySelector('#profile-edit-button');
