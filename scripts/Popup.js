4ce5cf1547bab1858bfc434f3627a91ea8d28c7c
Raeann Lytle <raelytle@gmail.com>
2023-03-16 15:54:27 -0400

    create popup.js and popupwithform.js

---
+++ scripts/Popup.js
@@ -0,0 +1,23 @@
+class Popup {
+  constructor({ popupSelector }) {
+    this._popupElement = document.querySelector(popupSelector);
+  }
+
+  open() {
+    // opens popup
+  }
+
+  close() {
+    // closes popup
+  }
+
+    _handleEscapeClose() {
+      // listens for escape keydown
+    }
+
+    setEventListeners() {
+      // sets event listeners
+    }
+
+
+}
