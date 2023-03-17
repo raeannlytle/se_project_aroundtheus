7a49cc15925657f937b16335e328a797927e27ca
Raeann Lytle <raelytle@gmail.com>
2023-03-16 21:57:25 -0400

    working on Popup

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -1,23 +1,37 @@
-class Popup {
+export default class Popup {
   constructor({ popupSelector }) {
     this._popupElement = document.querySelector(popupSelector);
   }
 
   open() {
-    // opens popup
+    this._popup.classList.add("modal_opened");
+    document.addEventListener("keydown", this._handleEscapeClose);
+    document.addEventListener("mousedown", this._handleOverlay);
   }
 
   close() {
-    // closes popup
+    this._popup.classList.remove("modal_opened");
+    document.addEventListener("keydown", this._handleEscapeClose);
+    document.addEventListener("mousedown", this._handleOverlay);
   }
 
-    _handleEscapeClose() {
-      // listens for escape keydown
+  _handleEscapeClose = (evt) => {
+    if (evt.key === "Escape") {
+      this.close();
     }
+  };
 
-    setEventListeners() {
-      // sets event listeners
+  _handleOverlay = (evt) => {
+    if (evt.target.classList.contains("modal_opened")) {
+      this.close();
     }
+  };
 
-
+  setEventListeners() {
+    this._popup.addEventListener("click", (evt) => {
+      if (evt.target.classList.contains("modal__close")) {
+        this.close();
+      }
+    });
+  }
 }
