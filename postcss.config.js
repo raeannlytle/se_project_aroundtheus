c53351964c17a9ddb792fc0ae1ac6544f47c69dc
Raeann Lytle <raelytle@gmail.com>
2023-03-19 21:31:58 -0400

    fix errors with npm run dev

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-// postcss.config.js
-
-// connect plugins to the file
-const autoprefixer = require("autoprefixer");
-const cssnano = require("cssnano");
-
 module.exports = {
-  // connect plugins to PostCSS
   plugins: [
-    // connect autoprefixer
-    autoprefixer,
-    // pass an object with options upon connecting cssnano:
-    cssnano({ preset: "default" }) // set default minification settings
+    require('autoprefixer'),
+    cssnano = require('cssnano'),
   ]
 };
