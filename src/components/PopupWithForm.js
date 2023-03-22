fd6271ee33e655adc389898098272b995a0add6f
Raeann Lytle <raelytle@gmail.com>
2023-03-21 21:40:49 -0400

    fix selector

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,8 +5,8 @@ export default class PopupWithForm extends Popup {
     super({ popupSelector });
 
     this._popupForm = this._popupElement.querySelector('.modal__form');
-    this._formInputs = this._popupForm.querySelectorAll('.modal__input');
-    this._handleFormSubmit = handleFormSubmit;
+    this._formInputs = this._popupForm.querySelectorAll('.modal__form-input');
+    this._handleFormSubmit = handleFormSubmit
   }
 
   _getInputValues() {
