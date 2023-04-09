0b58ff84a5f4a2db2718bd7d5072296592e44977
Raeann Lytle <raelytle@gmail.com>
2023-04-08 23:28:53 -0400

    fix the submit for confirmButton

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -4,21 +4,19 @@ export default class PopupWithConfirmation extends Popup {
   constructor(popupSelector) {
     super({ popupSelector });
     this._popupForm = this._popupElement.querySelector(".modal__form");
-    this._confirmButton = document.querySelector("#delete-confirm-button");
-    this._saveButton = document.querySelector(".modal__button");
+    this._confirmButton = this._popupForm.querySelector("#delete-confirm-button");
+    this._saveButton = this._popupForm.querySelector(".modal__button");
   }
 
   setSubmitAction(action) {
     this._handleSubmit = action;
   }
-  
+
   setEventListeners() {
     super.setEventListeners();
-    this._confirmButton.addEventListener("click", () => {
-      this._handleSubmit();
-    });
     this._popupForm.addEventListener("submit", (evt) => {
       evt.preventDefault();
+      this._handleSubmit();
     })
   }
 }
